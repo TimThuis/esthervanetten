7e5e150497f863a5cb6baff5dd20a46de1678471
TimThuis <tim.thuis@gmail.com>
2017-08-30 11:09:06 +0200

    update var names with jq and replace all es6 syntax

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-const pages = $('.pagina');
-const photos = document.querySelectorAll('.werk img');
-const activeTab = document.querySelector('#header .active');
-const photoContainer = document.querySelector('.foto-container');
-const wiper = document.querySelector('.wiper');
-const menu = $('.menu-mobile');
-let activeImg = "";
-const wiperTl = new TimelineMax({
-  paused: true,
-});
-const menuTl = new TimelineMax({
-  paused: true,
-});
+var pages = $('.pagina');
+var activeTab = $('#header .active');
+var activeImg = "";
 
+var wiperTl = new TimelineMax({
+  paused: true,
+});
+var menuTl = new TimelineMax({
+  paused: true,
+});
 
 // timelines
-wiperTl.to(wiper, 0.5, {
+wiperTl.to('.wiper', 0.5, {
   left: 0,
   right: 'auto',
   width: '100vw',
-}).to(wiper, 0, {
+}).to('.wiper', 0, {
   left: 'auto',
   right: 0,
-}).to(wiper, 0.5, {
+}).to('.wiper', 0.5, {
   width: 0,
 })
 
@@ -30,37 +26,26 @@ TweenMax.staggerFrom('.foto-container img', 0.25, {
   delay: 0.5,
 }, 0.1)
 
-// tabs.forEach((tab) => {
-//   tab.addEventListener('click', tabClicked)
-// })
-
 $('.tab-btn').each(function(index, tab) {
   tab.addEventListener('click', tabClicked);
 })
-
-// photos.forEach((photo) => {
-//   photo.addEventListener('click', openPhoto)
-// })
 
 $('.werk img').each(function(index, tab) {
   tab.addEventListener('click', openPhoto);
 })
 
-
 function openPhoto(event) {
-  let c = 'activeImage'
-  let el = event.target.classList;
-
+  var el = event.target;
   if (activeImg === "") {
-    el.add(c);
+    $(el).addClass('activeImage');
     activeImg = event.target;
   } else {
-  activeImg.classList.remove(c)
+  $(activeImg).removeClass('activeImage')
   if (activeImg === event.target) {
-      el.remove(c);
+      $(el).removeClass('activeImage');
       activeImg = ""
   } else {
-    el.add(c);
+    $(el).addClass('activeImage');
     activeImg = event.target;
     }
   }
@@ -77,17 +62,17 @@ function moveActiveTab(tab) {
 
   switch (tab.dataset.name) {
     case 'werk':
-    activeTab.style.left = 0
-    activeTab.style.width = tab.clientWidth + 'px';
-    break;
+      $('#header .active').css('left', 0);
+      $('#header .active').css('width', tab.clientWidth + 'px');
+      break;
     case 'over':
-    activeTab.style.left = '98px'
-    activeTab.style.width = tab.clientWidth + 'px';
-    break;
+      $('#header .active').css('left', '98px');
+      $('#header .active').css('width', tab.clientWidth + 'px');
+      break;
     case 'contact':
-    activeTab.style.left = '194px'
-    activeTab.style.width = tab.clientWidth + 'px';
-    break;
+      $('#header .active').css('left', '194px');
+      $('#header .active').css('width', tab.clientWidth + 'px');
+      break;
   }
 }
 
@@ -108,5 +93,9 @@ function showPage(data) {
 }
 
 function showMenuMobile() {
-  menu.classList.contains('show')? menu.classList.remove('show') : menu.classList.add('show');
+  if ($('.menu-mobile').hasClass('show')) {
+    $('.menu-mobile').removeClass('show')
+  } else {
+    $('.menu-mobile').addClass('show');
+  }
 }
